dab2c6b075af47a281c6928e10810d156539d7b9
arkholt <arkholt@gmail.com>
2016-08-08 10:49:43 -0400

    placed tags on top meta

--- system/themes/snippets/content-blog.php
+++ system/themes/snippets/content-blog.php
@@ -5,7 +5,10 @@
 <?php endif ?>
 <div class="<?php echo $yellow->page->getHtml("entryClass") ?>">
 <div class="entry-header"><h1><?php echo $yellow->page->getHtml("titleContent") ?></h1></div>
-<div class="entry-meta"><i class='fa fa-calendar'></i> <?php echo htmlspecialchars($yellow->page->getDate("published")) ?> <?php echo $yellow->text->getHtml("blogBy") ?> <?php $authorCounter = 0; foreach(preg_split("/,\s*/", $yellow->page->get("author")) as $author) { if(++$authorCounter>1) echo ", "; echo "<a href=\"".$yellow->page->getPage("blog")->getLocation(true).$yellow->toolbox->normaliseArgs("author:$author")."\">".htmlspecialchars($author)."</a>"; } ?></div>
+<div class="entry-meta"><p><i class='fa fa-calendar'></i> <?php echo htmlspecialchars($yellow->page->getDate("published")) ?></p> <!--<?php echo $yellow->text->getHtml("blogBy") ?>--> <p><?php $authorCounter = 0; foreach(preg_split("/,\s*/", $yellow->page->get("author")) as $author) { if(++$authorCounter>1) echo ", "; echo "<a href=\"".$yellow->page->getPage("blog")->getLocation(true).$yellow->toolbox->normaliseArgs("author:$author")."\">".htmlspecialchars($author)."</a>"; } ?></p>
+  <p><?php if($yellow->page->isExisting("tag")): ?>
+  <p><!--<?php echo $yellow->text->getHtml("blogTag") ?> --> <i class='fa fa-tag'></i> <?php $tagCounter = 0; foreach(preg_split("/,\s*/", $yellow->page->get("tag")) as $tag) { if(++$tagCounter>1) echo ", "; echo "<a href=\"".$yellow->page->getPage("blog")->getLocation(true).$yellow->toolbox->normaliseArgs("tag:$tag")."\">".htmlspecialchars($tag)."</a>"; } ?></p>
+  <?php endif ?></p></div>
 <div class="entry-content"><?php echo $yellow->page->getContent() ?></div>
 <div class="entry-footer">
 <?php if($yellow->page->isExisting("tag")): ?>
