9ab17aa44b0fa9d578290a353a7d4c0204d646ff
arkholt <arkholt@gmail.com>
2016-08-08 11:28:07 -0400

    removed author

--- system/themes/snippets/content-blogpages.php
+++ system/themes/snippets/content-blogpages.php
@@ -10,9 +10,9 @@
 <div class="<?php echo $page->getHtml("entryClass") ?>">
 <div class="entry-header"><h1><a href="<?php echo $page->getLocation(true) ?>"><?php echo $page->getHtml("title") ?></a></h1></div>
 <div class="entry-meta"><p><i class='fa fa-calendar'></i> <?php echo htmlspecialchars($page->getDate("published")) ?></p> <!--<?php echo $yellow->text->getHtml("blogBy") ?>-->
-  <p><?php $authorCounter = 0; foreach(preg_split("/,\s*/", $page->get("author")) as $author) { if(++$authorCounter>1) echo ", "; echo "<a href=\"".$yellow->page->getLocation(true).$yellow->toolbox->normaliseArgs("author:$author")."\">".htmlspecialchars($author)."</a>"; } ?></p>
+  <!--<p><?php $authorCounter = 0; foreach(preg_split("/,\s*/", $page->get("author")) as $author) { if(++$authorCounter>1) echo ", "; echo "<a href=\"".$yellow->page->getLocation(true).$yellow->toolbox->normaliseArgs("author:$author")."\">".htmlspecialchars($author)."</a>"; } ?></p>-->
   <p><?php if($yellow->page->isExisting("tag")): ?>
-  <p><i class='fa fa-tag'></i> <?php $tagCounter = 0; foreach(preg_split("/,\s*/", $yellow->page->get("tag")) as $tag) { if(++$tagCounter>1) echo ", "; echo "<a href=\"".$yellow->page->getPage("blogpages")->getLocation(true).$yellow->toolbox->normaliseArgs("tag:$tag")."\">".htmlspecialchars($tag)."</a>"; } ?></p>
+  <p><i class='fa fa-tag'></i> <?php $tagCounter = 0; foreach(preg_split("/,\s*/", $yellow->page->get("tag")) as $tag) { if(++$tagCounter>1) echo ", "; echo "<a href=\"".$yellow->page->getPage("blog")->getLocation(true).$yellow->toolbox->normaliseArgs("tag:$tag")."\">".htmlspecialchars($tag)."</a>"; } ?></p>
   <?php endif ?></p></div>
 <div class="entry-content"><?php echo $yellow->toolbox->createTextDescription($page->getContent(), $yellow->config->get("blogPageLength"), false, "<!--more-->", " <a href=\"".$page->getLocation(true)."\">".$yellow->text->getHtml("blogMore")."</a>") ?></div>
 </div>
